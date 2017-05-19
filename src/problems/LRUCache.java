640b67749698264d54d02d821c21903ccc40bc24
Sasanka Panguluri <itssasanka@gmail.com>
2017-05-19 15:31:29 -0700

    LRUCache

--- src/problems/LRUCache.java
+++ src/problems/LRUCache.java
@@ -1,45 +1,130 @@
 package problems;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by sasanka on 5/19/17.
  */
-public class LRUCache {
-    private int size;
-    private ArrayList<Integer> list;
-    private HashMap<Integer, Integer> map;
+class Element<T> {
+    private T value;
+    private Element<T> next;
 
-    public LRUCache(int size) {
-        this.size = size;
+    public Element(T val) {
+        this.value = val;
     }
 
-    public int getSize() {
-        return size;
+    public Element<T> getNext() {
+        return next;
     }
 
-    public List<Integer> getList() {
-        return list;
+    public T getValue() {
+        return value;
     }
 
-    public Map<Integer, Integer> getMap() {
-        return map;
+    public void setNext(Element<T> next) {
+        this.next = next;
+    }
+}
+
+class MyList<T> {
+    private Element<T> head;
+    private int maxSize;
+
+    public MyList(int size) {
+        this.maxSize = size;
     }
 
-    public boolean isFull(){
-        return this.list.size() == size;
-    }
-
-    public void insert(Integer val){
-        if (isFull()) {
-            
+    public void add(Element<T> e) {
+        if (head == null) {
+            this.head = e;
+        } else {
+            e.setNext(head);
+            head = e;
         }
     }
 
+    public void remove(Element<T> e) {
+        if (e == head) {
+            this.head = e.getNext();
+        } else {
+            Element<T> c = head;
+            while (c != null) {
+                if (c.getNext() == e) {
+                    c.setNext(e.getNext());
+                }
+                c = c.getNext();
+            }
+        }
+    }
+
+    public void print() {
+        Element<T> c = this.head;
+        while (c != null) {
+            System.out.print(" " + c.getValue() + " ");
+            c = c.getNext();
+        }
+    }
+
+    public void cutOff() {
+        Element<T> c = head;
+        for (int i = 1; i < this.maxSize; i++) {
+            c = c.getNext();
+        }
+        c.setNext(null);
+    }
+}
+
+public class LRUCache {
+    private int size;
+    private MyList<Integer> list;
+    private HashMap<Integer, Element<Integer>> map;
+
+    public LRUCache(int size) {
+        this.size = size;
+        this.list = new MyList<>(size);
+        this.map = new HashMap<>();
+    }
+
+    public void add(Integer i) {
+        Element<Integer> e = new Element<>(i);
+        Element<Integer> existingElement = map.get(i);
+        if (existingElement != null) {
+            list.remove(existingElement);
+            map.remove(i);
+        }
+
+        list.add(e);
+        map.put(i, e);
+
+        if (map.size() > size) {
+            list.cutOff();
+        }
+
+    }
+
+    public void retrieve() {
+        this.list.print();
+    }
 
 
+    // 1 2 3 4 5 4 6
+    // With a size of 4, the LRU cache in the end must contain:
+    //      6453
+    // Steps:
+    // 4 3 2 1
+    // 5 4 3 2
+    // 4 5 3 2
+    // 6 4 5 3
+    public static void main(String args[]) {
+        LRUCache l = new LRUCache(4);
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        l.add(4);
+        l.add(6);
 
+        l.retrieve();
+    }
 }
